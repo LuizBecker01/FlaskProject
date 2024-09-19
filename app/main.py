fbd96bb52124851e99ff1bcba02475a436f0e6fe
LuizBecker01 <luizhenriquebecker000@gmail.com>
2024-09-19 10:15:31 -0300

    Day 19/09/24
    Ajustes aos models

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, Blueprint
+from flask import Flask, jsonify, request, render_template, Blueprint, current_user
 from flask_sqlalchemy import SQLAlchemy
 from models import User
 from db import db
@@ -14,6 +14,7 @@ blueprint = Blueprint('api', __name__, url_prefix='/api')
 def homepage():
     return render_template ("index.html")
 
+# Rota para criar um novo usuário
 @blueprint.route('/create_user', methods=['POST'])
 def create_user():
     username = request.form['username']
@@ -25,7 +26,6 @@ def create_user():
     if user:
         return jsonify(message='Usuário já existe!', created=0)
 
-    # Cria um novo usuário
     new_user = User(username=username, password=password)
     
     # Adiciona o novo usuário ao banco de dados
@@ -33,6 +33,20 @@ def create_user():
     db.session.commit()
 
     return jsonify(message='Usuário '+username+' criado com sucesso!', created=1)
+
+# Rota para deletar um usuário
+@blueprint.route('/delte_user', methods=['POST'])
+def delete_user():
+    username = request.form['username']
+    if current_user.username == username:
+        return jsonify(message='Voce não pode deletar o usuário atual', deleted=0)
+    else:
+        user = User.query.filter_by(username=username).first()
+        
+        db.session.delete(user)
+        db.session.commit()
+
+        return jsonify(message='Usuário '+username+' deletado com sucesso!', deleted=1)
 
 if __name__ == "__main__":
     app.run(debug=True)
