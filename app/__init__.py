2b8c7fa7e77d80d4652fc4994a69e9eed15119dc
LuizBecker01 <luizhenriquebecker000@gmail.com>
2024-10-01 18:03:17 -0300

    Day 01/10/24
    Ajustando Blueprints...

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('config.config')
+db = SQLAlchemy()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2404@localhost:5432/CNMD'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def create_app():
+    app = Flask(__name__)
 
-db = SQLAlchemy(app)
+    # Configs do app
+    app.config.from_object('config.config')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2404@localhost:5432/CNMD'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Importa e registra o Blueprint
-from app.main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+    # Inicializa o SQLAlchemy com app
+    db.init_app(app)
+
+    # Importa e registra o Blueprints
+
+    from app.main import main as main_blueprint
+    from app.main import blueprint as api_blueprint
+
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(api_blueprint)
+
+    return app
--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.attributes import flag_modified
 from app.models import User
 
+main = Blueprint('main', __name__)
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+
 app = Flask(__name__)
 
 # Definir a URI de conexão com o banco de dados
@@ -22,18 +25,14 @@ session = Session()
 
 Base.metadata.create_all(engine)
 
-blueprint = Blueprint('api', __name__, url_prefix='/api')
-
-main = Blueprint('main', __name__)
-
 @app.route('/')  
 def index():
     users = User.query.all()
     return render_template('index.html', users=users)
 
-@blueprint.route('/about')
-def about():
-    return 'About Page'
+# @blueprint.route('/about')
+# def about():
+#     return 'About Page'
 
 # Rota para criar um novo usuário
 @blueprint.route('/api/create_user', methods=['POST'])
@@ -56,18 +55,17 @@ def create_user():
     return jsonify(message='Usuário '+username+' criado com sucesso!', created=1)
 
 # Rota para deletar um usuário
-@blueprint.route('/api/delete_user', methods=['POST'])
-def delete_user():
-    username = request.form['username']
-    if current_user.username == username:
-        return jsonify(message='Voce não pode deletar o usuário atual', deleted=0)
-    else:
-        user = User.query.filter_by(username=username).first()
-        
-        db_.session.delete(user)
-        db_.session.commit()
+# @blueprint.route('/api/delete_user', methods=['POST'])
+# def delete_user():
+#     username = request.form['username']
+#     if current_user.username == username:
+#         return jsonify(message='Voce não pode deletar o usuário atual', deleted=0)
+    
+#     user = User.query.filter_by(username=username).first()    
+#     db_.session.delete(user)
+#     db_.session.commit()
 
-        return jsonify(message='Usuário '+username+' deletado com sucesso!', deleted=1)
+#     return jsonify(message='Usuário '+username+' deletado com sucesso!', deleted=1)
 
 # app.register_blueprint(blueprint)
 
